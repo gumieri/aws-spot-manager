67ee067436f2034bc89fd416a46c7969f4995b60
Rafael Gumieri <rgumieri@gmail.com>
2018-11-25 09:56:24 -0200

    ref: remove userData from command

--- fleet/request.js
+++ fleet/request.js
@@ -7,51 +7,9 @@ const ecs = new AWS.ECS({ apiVersion: '2014-11-13' })
 const iam = new AWS.IAM({ apiVersion: '2010-05-08' })
 
 const config = require('../lib/config')
+const { spotFleet: spotFleetUserData } = require('../lib/user_data')
 const extendedSource = require('../lib/extended_source')
 const { stringArrayOrEmpty, requiredInput } = require('../lib/utils')
-
-function spotFleetUserData ({ cluster }) {
-  return `#!/bin/bash
-echo ECS_CLUSTER=${cluster} >> /etc/ecs/ecs.config
-echo ECS_BACKEND_HOST= >> /etc/ecs/ecs.config
-export PATH=/usr/local/bin:$PATH
-yum -y install jq
-easy_install pip
-pip install awscli
-aws configure set default.region ${AWS.config.region}
-cat <<EOF > /etc/init/spot-instance-termination-notice-handler.conf
-description "Start spot instance termination handler monitoring script"
-author "Amazon Web Services"
-start on started ecs
-script
-echo \$\$ > /var/run/spot-instance-termination-notice-handler.pid
-exec /usr/local/bin/spot-instance-termination-notice-handler.sh
-end script
-pre-start script
-logger "[spot-instance-termination-notice-handler.sh]: spot instance termination
-notice handler started"
-end script
-EOF
-cat <<EOF > /usr/local/bin/spot-instance-termination-notice-handler.sh
-#!/bin/bash
-while sleep 5; do
-	if [ -z \$(curl -Isf http://169.254.169.254/latest/meta-data/spot/termination-time)]; then
-		/bin/false
-	else
-		logger "[spot-instance-termination-notice-handler.sh]: spot instance termination notice detected"
-		STATUS=DRAINING
-		ECS_CLUSTER=\$(curl -s http://localhost:51678/v1/metadata | jq .Cluster | tr -d \")
-		CONTAINER_INSTANCE=\$(curl -s http://localhost:51678/v1/metadata | jq .ContainerInstanceArn | tr -d \")
-		logger "[spot-instance-termination-notice-handler.sh]: putting instance in state \$STATUS"
-		/usr/local/bin/aws  ecs update-container-instances-state --cluster \$ECS_CLUSTER --container-instances \$CONTAINER_INSTANCE --status \$STATUS
-		logger "[spot-instance-termination-notice-handler.sh]: putting myself to sleep..."
-		sleep 120 # exit loop as instance expires in 120 secs after terminating notification
-	fi
-done
-EOF
-chmod +x /usr/local/bin/spot-instance-termination-notice-handler.sh
-`
-}
 
 async function findSubnet ({ Subnet }) {
   const { Subnets: byId } = await ec2
@@ -194,9 +152,10 @@ module.exports = async ({
   }
 
   if (!userData && ecsCluster) {
-    userData = Buffer.from(spotFleetUserData({ cluster: ecsCluster })).toString(
-      'base64'
-    )
+    userData = spotFleetUserData({
+      cluster: ecsCluster,
+      region: AWS.config.region
+    })
   }
 
   const SpotPrice = spotPrice
